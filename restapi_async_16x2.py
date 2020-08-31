1e1c82bbb971f96d260e8f280ac2a61aeee2acf0
pi <get4gopim@gmail.com>
2020-08-31 21:27:12 +0530

    fetch all data from BeautifulSoup html scraper library

--- restapi_async_16x2.py
+++ restapi_async_16x2.py
@@ -116,7 +116,7 @@ def update_weather_preciption():
 
     # Make string 16 chars only and left justify with space if length is less.
     line2 = preciption[0:lcd_disp_length]
-    line2 = line2.ljust(lcd_disp_length, ' ')
+    line2 = line2.just(lcd_disp_length, ' ')
 
 # update display line strings
 def update_rate_line():
