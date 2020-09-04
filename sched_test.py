72a6bcb577cc9b21732d8ff5bfc8f2d677f98ff3
pi <get4gopim@gmail.com>
2020-09-04 22:48:15 +0530

    scheduler method proper scheduling

--- sched_test.py
+++ sched_test.py
@@ -15,9 +15,15 @@ def job_min():
     LOGGER.info ("Min stuff...\n\n")
 
 
-schedule.every(.9).seconds.do(job)
-schedule.every().minute.do(job_min)
-schedule.every().day.at("10:30").do(job)
+# schedule.every(.9).seconds.do(job)
+# schedule.every().minute.do(job_min)
+# schedule.every().minute.at(":21").do(job)
+
+schedule.every().monday.tuesday.wednesday.thursday.friday.saturday \
+        .at("06:30").at("07:00").at("07:30").do(job)
+
+# schedule.every(30).to(50).minutes.do(job)
+# schedule.every(5).to(10).seconds.do(job)
 
 LOGGER.info("starting \n\n")
 
