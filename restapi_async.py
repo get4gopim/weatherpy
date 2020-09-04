87f3abb9adda9efa184886090c6c7406a98b3f71
pi <get4gopim@gmail.com>
2020-09-04 21:40:00 +0530

    scheduler method call gold & weather apis

--- restapi_async.py
+++ restapi_async.py
@@ -433,7 +433,7 @@ if __name__ == '__main__':
     rand_bool = True
     time.sleep(service_start_time_in_secs)
 
-    schedule.every(.7).seconds.do(every_second)
+    schedule.every(.7).seconds.do(jobqueue.put, every_second)
     schedule.every(2).minutes.do(jobqueue.put, call_weather_api)
     schedule.every(3).minutes.do(jobqueue.put, call_gold_api)
 
