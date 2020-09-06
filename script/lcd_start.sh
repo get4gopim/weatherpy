04278fc1e8564a0f9694dde8f95dab97eea0247a
pi <get4gopim@gmail.com>
2020-09-06 13:02:31 +0530

    startup script added

---
+++ script/lcd_start.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo "Starting LCD Script"
+
+echo "$JAVA_HOME/bin"
+
+# network wait sleep time [if required]
+
+#sleep 10
+
+echo 'Starting python lcd script'
+
+python3 /home/pi/workspace/weatherpy/restapi_async.py > /home/pi/i2c_lcd.log 2>&1 &
+
+# sleep and try the below after 10 mins to pickup the changes from git
+
+sleep 600
+
+echo 'Git fetch update will start'
+
+cd /home/pi/workspace/weatherpy
+git fetch --all
+git reset --hard origin/master
+git pull origin master
+
+echo 'Git fetch & update completed'
+
+echo 'Done'
