3ea84b18e2c62125f2569152c4f1619c3701b435
pi <get4gopim@gmail.com>
2020-09-10 20:54:10 +0530

    run weather & gold in a seperate thread try

--- restapi_async.py
+++ restapi_async.py
@@ -335,12 +335,11 @@ def run_threaded(job_vars):
 
 def add_scheduler(location):
     # Update time every second
-    # schedule.every(1).seconds.do(jobqueue.put, (every_second, []))
-    schedule.every(1).seconds.do(run_threaded, (every_second, []))
+    schedule.every(1).seconds.do(jobqueue.put, (every_second, []))
 
     # Update weather every 15 mins once every day
-    schedule.every(1).minutes.do(jobqueue.put, (call_weather_api, [location]))
-    schedule.every(1).minutes.do(jobqueue.put, (call_gold_api, []))
+    schedule.every(1).minutes.do(run_threaded, (call_weather_api, [location]))
+    schedule.every(1.5).minutes.do(run_threaded, (call_gold_api, []))
 
     # Update gold rate every 1 hour except sunday from 10 AM to 5 PM
     gold_times = ["09:30", "10:00", "11:00", "12:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "17:00"]
