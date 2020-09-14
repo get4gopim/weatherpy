5e593914c18c6cf0debf5824e6952cb582d40cd5
pi <get4gopim@gmail.com>
2020-09-14 19:54:47 +0530

    sync manually from master

--- script/lcd_start.sh
+++ script/lcd_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting LCD Script"
 
-echo "$JAVA_HOME/bin"
+# echo "$JAVA_HOME/bin"
 
 # network wait sleep time [if required]
 
@@ -9,7 +9,15 @@ echo "$JAVA_HOME/bin"
 
 echo 'Starting python lcd script'
 
-python3 /home/pi/workspace/weatherpy/20x4_lcd_main.py > /home/pi/i2c_lcd.log 2>&1 &
+#location='49ceef3a36a9f805cdbf9fb01da64837d038f2256d4c3457dca28833422e0000' # Kolathur
+location='4ef51d4289943c7792cbe77dee741bff9216f591eed796d7a5d598c38828957d' # Thalambur
+#location='kolathur+salem'
+
+echo $location
+
+#python3 /home/pi/workspace/weatherpy/16x2_lcd_main.py $location > /home/pi/i2c_lcd.log 2>&1 &
+python3 /home/pi/workspace/weatherpy/20x4_lcd_main.py $location > /home/pi/i2c_lcd.log 2>&1 &
+#python3 /home/pi/workspace/weatherpy/2in13_epaper_main.py $location > /home/pi/i2c_lcd.log 2>&1 &
 
 # sleep and try the below after 10 mins to pickup the changes from git
 
@@ -17,10 +25,10 @@ sleep 600
 
 echo 'Git fetch update will start'
 
-cd /home/pi/workspace/weatherpy
-git fetch --all
-git reset --hard origin/master
-git pull origin master
+#cd /home/pi/workspace/weatherpy
+#git fetch --all
+#git reset --hard origin/master
+#git pull origin master
 
 echo 'Git fetch & update completed'
 
